503e6e792b6caa5d2365c8f982e519b43d5d262f
Artur Yurchenko <artur24814@gmail.com>
2025-01-08 10:29:39 +0100

    The project has been initialized and the task for Lab 9-1 has been completed

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import exeptionsGens.NullPointerExceptionGenerator;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            NullPointerExceptionGenerator.raiseNullPointerException();
+        } catch (NullPointerException e){
+            System.out.println("Złapano NullPointerException");
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+}
---
+++ src/main/java/exeptionsGens/NullPointerExceptionGenerator.java
@@ -0,0 +1,7 @@
+package exeptionsGens;
+
+public class NullPointerExceptionGenerator {
+    public static void raiseNullPointerException () throws NullPointerException {
+        throw new NullPointerException();
+    }
+}
