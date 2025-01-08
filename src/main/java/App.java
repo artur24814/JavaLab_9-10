6a68b929db1d2dd049dfd10d28e5e4ee4e8326fe
Artur Yurchenko <artur24814@gmail.com>
2025-01-09 00:09:36 +0100

    Sample transfer handling has been added to the main method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import exceptionsGen.NullPointerExceptionGenerator;
+import account.*;
 
 public class App {
     public static void main(String[] args) {
@@ -8,6 +9,21 @@ public class App {
             System.out.println("Złapano NullPointerException");
             e.printStackTrace();
             System.out.println(e.toString());
+        }
+
+        Account senderAccount = new Account("Jan Kowalski", 500, "12345");
+        Account receiverAccount = new Account("Anna Nowak", 1000, "67890");
+
+        try {
+            System.out.println("Próba przelewu 1000 zł z konta o saldzie: " + senderAccount.getBalance());
+            senderAccount.transfer(receiverAccount, 1000);
+        } catch (NotEnoughMoneyException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Wystąpił nieoczekiwany błąd: " + e.getMessage());
+        } finally {
+            System.out.println("Stan konta nadawcy: " + senderAccount.getBalance());
+            System.out.println("Stan konta odbiorcy: " + receiverAccount.getBalance());
         }
     }
 }
